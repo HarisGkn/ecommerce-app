501337b9677771c1b6304d9e6139242004cebb41
HarisGkn <haris.gjo@gmail.com>
2021-03-26 14:51:49 +0200

    Added product page and added some styling to home page

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,40 @@
+import Product from './Product'
+import home from './img/home.jpg'
+import laptop from './img/laptop.jpg'
+import smartphone from './img/smartphone.jpg'
+import camera from './img/camera.jpg'
+import "./Home.css"
+
 
 function Home(){
     return(
-        <h1>Home</h1>
+        <div className="home">
+            <img className="home__image" src={home} alt="logo"/>
+
+            <div className="home__row">
+                <Product
+                    id="000001"
+                    title="Laptop "
+                    price={1499.99}
+                    rating={5}
+                    image={laptop}
+                />
+                <Product
+                    id="000002"
+                    title="Smartphone"
+                    price={349.99}
+                    rating={4}
+                    image={smartphone}
+                />
+                <Product
+                    id="000003"
+                    title="Camera"
+                    price={534.99}
+                    rating={5}
+                    image={camera}
+                />
+            </div>
+        </div>
     )
 }
 export default Home
