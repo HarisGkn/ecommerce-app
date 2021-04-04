1bf586bdc476ba8e737ca68eacb294514641d6b3
HarisGkn <haris.gjo@gmail.com>
2021-04-04 17:13:18 +0300

    minor changes in footer

--- src/Footer.js
+++ src/Footer.js
@@ -5,20 +5,40 @@ function Footer(){
 
     return(
             <div className="footerArea">
-                <div className="footerArea__top">
-                    <p>back to top</p>
+                <div className="footerArea__links">
+                    <div className="footerArea__linkArea">
+                        <span>About Us</span>
+                    </div>
+                    <div className="footerArea__linkArea">
+                        <span>Payment Methods</span>
+                    </div>
+                    <div className="footerArea__linkArea">
+                        <span>Delivery rates and policy</span>
+                    </div>
                 </div>
                 <div className="footerArea__links">
                     <div className="footerArea__linkArea">
-                        <span>test1</span>
+                        <span>Returns and replacements</span>
                     </div>
                     <div className="footerArea__linkArea">
-                        <span>test</span>
+                        <span>Environmental policy</span>
                     </div>
                     <div className="footerArea__linkArea">
-                        <span>test</span>
+                        <span>Customer service</span>
                     </div>
                 </div>
+                <div className="footerArea__links">
+                    <div className="footerArea__linkArea">
+                        <span>Company location</span>
+                    </div>
+                    <div className="footerArea__linkArea">
+                        <span>Contact info</span>
+                    </div>
+                    <div className="footerArea__linkArea">
+                        <span>Covid-19</span>
+                    </div>
+                </div>
+                
             </div>
     )
 
